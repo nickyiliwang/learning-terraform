74402a93fa291a1366dbe6d2b8290f4e1e02660b
nickyiliwang <yw12me@gmail.com>
2021-12-13 18:51:52 +0000

    code organization for main.tf, separated into var, outputs and provider files

--- 2-modules/main.tf
+++ 2-modules/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.15.0"
-    }
-  }
-}
-
-provider "docker" {}
-
 // Local-exec provisioner
 resource "null_resource" "dockervolume" {
   provisioner "local-exec" {
@@ -20,51 +9,6 @@ resource "null_resource" "dockervolume" {
     // checking if the folder exists
     command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
   }
-}
-
-# Variables
-
-variable "image" {
-  type        = map(any)
-  description = "image for container depending on environment"
-  default = {
-    DEV  = "nodered/node-red:latest"
-    PROD = "nodered/node-red:latest-minimal"
-  }
-}
-
-variable "int_port" {
-  type    = number
-  default = 1880
-
-
-  validation {
-    condition     = var.int_port == 1880
-    error_message = "The internal port must be 1880."
-  }
-}
-
-variable "ext_port" {
-  type = map(any)
-
-
-  # validating different ports for different env by accessing the ext_port map
-  # DEV
-  validation {
-    condition     = max(var.ext_port["DEV"]...) <= 65535 && min(var.ext_port["DEV"]...) >= 1980
-    error_message = "Must provide valid external port range 0 - 65535."
-  }
-  # PROD
-  validation {
-    condition     = max(var.ext_port["PROD"]...) <= 65535 && min(var.ext_port["PROD"]...) >= 1880
-    error_message = "Must provide valid external port range 0 - 65535."
-  }
-
-}
-
-locals {
-  // lookup the map of ext_ports and get how many pods we are deploying with open ports
-  container_count = length(var.ext_port[terraform.workspace])
 }
 
 resource "docker_image" "nodered_image" {
@@ -100,17 +44,4 @@ resource "docker_container" "nodered_container" {
     // Use path.cwd to get the fs path of the current working directory
     host_path = "${path.cwd}/noderedvol"
   }
-}
-
-# OutPuts
-output "container-names" {
-  # we are using the splat ([*]) syntax here to make our output DRY
-  value       = docker_container.nodered_container[*].name
-  description = "The name of the container"
-}
-
-output "local-ip-plus-external-ports" {
-  # we are using a for loop + the join function to keep the code DRY
-  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address, i.ports[0].external])]
-  description = "Local ip:external-port"
 }
