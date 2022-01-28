8ef2445da3cad3dcbc6977c348eecad7c7db4273
nickyiliwang <yw12me@gmail.com>
2022-01-28 21:36:00 +0000

    k8s terraform deployment

--- 4-K8/terraform-k8s/locals.tf
+++ 4-K8/terraform-k8s/locals.tf
@@ -1,31 +1,26 @@
 # root/locals.tf
 
 locals {
-deployment = {
+  deployment = {
     nodered = {
-        image: nodered/node-red:latest
-int = 1880
-ext = 1880
-volumepath = "/data"
-
+      image      = "nodered/node-red:latest"
+      int        = 1880
+      ext        = 1880
+      volumepath = "/data"
+    }
+    influxdb = {
+      image      = "influxdb"
+      int        = 8086
+      ext        = 8086
+      volumepath = "/var/lib/influxdb"
+    }
+    grafana = {
+      image      = "grafana/grafana"
+      int        = 3000
+      ext        = 3000
+      volumepath = "/var/lib/grafana"
     }
 
-influxdb = {
-    image: "influxdb"
-int = 8086
-ext = 8086
-volumepath = "/var/lib/influxdb"
-}
-
-
-grafana = {
-    
-image: "grafana/grafana"
-int = 3000
-ext = 3000
-volumepath = "/var/lib/grafana"
-}
-
-}
+  }
 }
 
