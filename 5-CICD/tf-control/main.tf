ba8a38b1891d53bef58c99954634a6c24a56fdc4
nickyiliwang <yw12me@gmail.com>
2022-02-01 09:42:50 +0000

    added resources to create a github repo with terraform template

---
+++ 5-CICD/tf-control/main.tf
@@ -0,0 +1,27 @@
+# root/main.tf
+
+resource "github_repository" "tf_repo" {
+  name             = "tf-dev-repo"
+  description      = "Networking and Compute Resources"
+  auto_init        = true
+  license_template = "mit"
+
+  visibility = "private"
+}
+
+resource "github_branch" "default" {
+  repository = github_repository.tf_repo.name
+  branch     = "main"
+}
+
+resource "github_repository_file" "maintf" {
+  repository          = github_repository.tf_repo.name
+  branch              = "main"
+  file                = "main.tf"
+  content             = file("./deployments/tf-dev/main.tf")
+  commit_message      = "Managed by Terraform"
+  commit_author       = "nick"
+  commit_email        = "nickyiliwang01@gmail.com"
+  overwrite_on_create = true
+}
+
